4c59a33190993528590ef45a6336240cb94065f6
Dhekki <gmdo0420@gmail.com>
2025-11-30 18:52:54 +0000

    :bug: fix: Corrige lógica do deploy e remove configuração obsoleta do compose.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd /home/ec2-user/Cantina_Vidal || echo "Repositório não encontrado!" && exit
+cd /home/ec2-user/Cantina_Vidal || { echo "Repositório não encontrado!"; exit 1; }
 
+echo "Entrou na pasta. Baixando atualizações..."
 git pull origin main
 
+echo "Recriando containers..."
 docker compose up -d --build
 
+echo "Limpando imagens antigas..."
 docker image prune -f
 
 echo "Deploy concluído!"
