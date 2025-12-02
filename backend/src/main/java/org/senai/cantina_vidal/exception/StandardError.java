ee7ae88d618345709f97d97823d5821e9dec41d9
Gabriel Oliveira <gmdo0420@gmail.com>
2025-12-02 20:56:36 -0300

    :sparkles: feat: Adiciona exceção no handler global para `@Valid` e builder para StandardError.

--- backend/src/main/java/org/senai/cantina_vidal/exception/StandardError.java
+++ backend/src/main/java/org/senai/cantina_vidal/exception/StandardError.java
@@ -1,27 +1,28 @@
 package org.senai.cantina_vidal.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Map;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StandardError implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    private Map<String, String> validationErrors;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "GMT")
     private LocalDateTime timestamp;
+    private String message;
     private Integer status;
     private String error;
-    private String message;
     private String path;
 }
