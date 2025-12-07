f7272785e14734027f1aa6aeec0f4f56b0fd2b86
Gabriel Oliveira <gmdo0420@gmail.com>
2025-12-07 19:42:03 -0300

    :sparkles: feat: Implementa lógica de autenticação JWT e controle de acesso (RBAC).

--- backend/src/main/java/org/senai/cantina_vidal/repository/RefreshTokenRepository.java
+++ backend/src/main/java/org/senai/cantina_vidal/repository/RefreshTokenRepository.java
@@ -1,10 +1,14 @@
 package org.senai.cantina_vidal.repository;
 
-import org.senai.cantina_vidal.entity.Product;
 import org.senai.cantina_vidal.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    void deleteByUserId(Long userId);
+
+    Optional<RefreshToken> findByToken(String token);
 }
