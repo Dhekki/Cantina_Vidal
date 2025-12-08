c4ca52eddcc888361a5bc89840328092982f219f
Gabriel Oliveira <gmdo0420@gmail.com>
2025-12-08 00:09:30 -0300

    :sparkles: feat: Ativa segurança por método, desativa segurança padrão do SpringSecurity e restringe acesso a dados de usuário.

--- backend/src/main/java/org/senai/cantina_vidal/CantinaVidalApplication.java
+++ backend/src/main/java/org/senai/cantina_vidal/CantinaVidalApplication.java
@@ -4,8 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @OpenAPIDefinition(
         info = @Info(
                 title = "Cantina Vidal",
--- backend/src/main/java/org/senai/cantina_vidal/controller/UserController.java
+++ backend/src/main/java/org/senai/cantina_vidal/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.senai.cantina_vidal.dto.UserResponseDTO;
 import org.senai.cantina_vidal.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class UserController {
     private final UserService service;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
     ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(new UserResponseDTO(service.findById(id)));
     }
