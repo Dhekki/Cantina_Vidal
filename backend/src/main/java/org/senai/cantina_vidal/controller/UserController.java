63f0602ae90a127f3e5a347409ab8f04bf8e369a
Gabriel Oliveira <gmdo0420@gmail.com>
2025-12-09 17:28:34 -0300

    :sparkles: feat: Adiciona endpoint /me.

--- backend/src/main/java/org/senai/cantina_vidal/controller/UserController.java
+++ backend/src/main/java/org/senai/cantina_vidal/controller/UserController.java
@@ -2,9 +2,11 @@ package org.senai.cantina_vidal.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.senai.cantina_vidal.dto.UserResponseDTO;
+import org.senai.cantina_vidal.entity.User;
 import org.senai.cantina_vidal.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,11 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
     ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(new UserResponseDTO(service.findById(id)));
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    ResponseEntity<UserResponseDTO> me(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(new UserResponseDTO(user));
     }
 }
